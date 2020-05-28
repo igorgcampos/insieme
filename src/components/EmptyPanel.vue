5a4f4a6a3ec0be9eb04a23548123239106809624
Julio Nobrega <JNobrega@TELESPAZIO.local>
2020-05-28 19:58:23 -0300

    Painel de aviso para resultados vazios.

--- src/components/EmptyPanel.vue
+++ src/components/EmptyPanel.vue
@@ -1,6 +1,17 @@
 <template>
 
-  <div></div>
+  <v-col>
+    <v-row
+      justify="center"
+      class="mb-3"
+    >
+      <v-icon x-large>mdi-help-circle</v-icon>
+    </v-row>
+    <v-row justify="center">
+      <span class="text-center headline font-weight-bold grey--text text--darken-3">
+        {{message}} </span>
+    </v-row>
+  </v-col>
 </template>
 
 <script>
