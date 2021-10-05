b984336bdc3b521a76f7c4ce6d4600977a6d87f7
JNobrega <julio.nobrega@telespazio.com>
2021-10-05 12:40:31 -0300

    Adicionando imagem do mateo no InfoDialog.vue

--- src/components/dialogs/InfoDialog.vue
+++ src/components/dialogs/InfoDialog.vue
@@ -3,7 +3,25 @@
     v-model="dialog"
     max-width="350"
   >
-    <v-card class="mt-2">
+    <v-card class="pt-6">
+
+      <v-lazy
+        :options="{
+            threshold: .6
+            }"
+        transition="slide-x-transition"
+      >
+        <div class="ma-0 pa-0">
+          <v-img
+            src="../../assets/robo.png"
+            max-height="100"
+            max-width="100"
+            class="mx-auto mb-6"
+          ></v-img>
+
+        </div>
+      </v-lazy>
+
       <v-card-title
         style="word-break: normal; !important"
         class="headline"
