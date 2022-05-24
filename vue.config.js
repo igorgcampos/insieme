2a8bd2ef9f906da0daebdf0ad8bccb432fadbdac
JNobrega <camilaS0#>
2022-05-24 09:22:07 -0300

    Adicionando compressão gzip

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,31 @@
+const zlib = require("zlib");
 module.exports = {
   "transpileDependencies": [
     "vuetify"
   ],
   devServer: {
     port: 8081
+  },
+  pluginOptions: {
+    compression:{
+      modes: ['production'],
+      brotli: {
+        filename: '[file].br[query]',
+        algorithm: 'brotliCompress',
+        include: /\.(js|css|html|svg|json)(\?.*)?$/i,
+        compressionOptions: {
+          params: {
+            [zlib.constants.BROTLI_PARAM_QUALITY]: 11,
+          },
+        },
+        minRatio: 0.8,
+      },
+      gzip: {
+        filename: '[file].gz[query]',
+        algorithm: 'gzip',
+        include: /\.(js|css|html|svg|json)(\?.*)?$/i,
+        minRatio: 0.8,
+      }
+    }
   }
 }
