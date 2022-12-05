b024713416ac71d8fb0c16189761115bec423dec
JNobrega <camilaS0#>
2022-12-05 10:08:27 -0300

    noProfile: () => (this.$getUser()

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -1,16 +1,8 @@
 <template>
   <v-content>
-    <v-container
-      fluid
-      fill-height
-      class="grey lighten-5 pa-0"
-    >
+    <v-container fluid fill-height class="grey lighten-5 pa-0">
       <v-layout row>
-        <v-flex
-          xs12
-          sm4
-          class="d-none d-sm-flex"
-        >
+        <v-flex xs12 sm4 class="d-none d-sm-flex">
           <v-layout column>
             <v-img
               src="../assets/satelites.jpg"
@@ -27,50 +19,52 @@
                       class="mx-auto"
                     ></v-img>
                   </div>
-                  <div class="display-1 text-xs-center font-weight-black mb-3 white--text">
-                    {{$vuetify.lang.t('$vuetify.BEM_VINDO')}}
+                  <div
+                    class="
+                      display-1
+                      text-xs-center
+                      font-weight-black
+                      mb-3
+                      white--text
+                    "
+                  >
+                    {{ $vuetify.lang.t("$vuetify.BEM_VINDO") }}
                   </div>
-                  <span class="subheading white--text">{{$vuetify.lang.t('$vuetify.MENSAGEM_INICIAL')}}</span>
-                  <div style="margin-top:45%;">
+                  <span class="subheading white--text">{{
+                    $vuetify.lang.t("$vuetify.MENSAGEM_INICIAL")
+                  }}</span>
+                  <div style="margin-top: 45%">
                     <v-btn
                       outlined
                       color="white"
                       small
                       @click="openVersionDialog = true"
                     >
-                      {{$vuetify.lang.t('$vuetify.VERSAO')+ $getVersion()}}
+                      {{ $vuetify.lang.t("$vuetify.VERSAO") + $getVersion() }}
                     </v-btn>
                   </div>
                 </div>
               </div>
             </v-img>
-
           </v-layout>
         </v-flex>
 
-        <v-flex
-          class="active horizontal-center"
-          fill-height
-        >
-          <v-layout
-            column
-            align-center
-            full-width
-            class="vertical-center"
-          >
-            <v-flex
-              xs12
-              mb-3
-            >
+        <v-flex class="active horizontal-center" fill-height>
+          <v-layout column align-center full-width class="vertical-center">
+            <v-flex xs12 mb-3>
               <div class="login-wrapper text-xs-center">
-                <div class="display-2 font-weight-black red--text text--darken-1">Insieme</div>
+                <div
+                  class="display-2 font-weight-black red--text text--darken-1"
+                >
+                  Insieme
+                </div>
               </div>
             </v-flex>
 
-            <v-flex
-              xs24
-              mb-4
-            > <span class="grey--text text--lighten-1">{{$vuetify.lang.t('$vuetify.MENSAGEM_LOGIN')}}</span>
+            <v-flex xs24 mb-4>
+              <span class="grey--text text--lighten-1">{{
+                $vuetify.lang.t("$vuetify.MENSAGEM_LOGIN")
+              }}</span>
             </v-flex>
             <v-flex>
               <v-text-field
@@ -92,14 +86,17 @@
                 single-line
                 solo
                 ref="password"
-                :rules="success?[rules.emailMatch, rules.noProfile]:[rules.emailMatch]"
+                :rules="
+                  success
+                    ? [rules.emailMatch, rules.noProfile]
+                    : [rules.emailMatch]
+                "
                 :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
                 prepend-inner-icon="mdi-lock"
                 :type="show ? 'text' : 'password'"
                 @click:append="show = !show"
                 @keydown.enter="login()"
               ></v-text-field>
-
             </v-flex>
             <v-btn
               large
@@ -110,7 +107,8 @@
               id="sign-up"
               :loading="loading"
               @click="login()"
-            >{{$vuetify.lang.t('$vuetify.ENTRAR')}}</v-btn>
+              >{{ $vuetify.lang.t("$vuetify.ENTRAR") }}</v-btn
+            >
 
             <v-row class="ma-3">
               <v-chip
@@ -137,7 +135,6 @@
                 English
               </v-chip>
             </v-row>
-
           </v-layout>
         </v-flex>
       </v-layout>
@@ -151,16 +148,14 @@
 </template>
 
 <script>
-
-import VersionsDialog from '../components/dialogs/VersionsDialog';
+import VersionsDialog from "../components/dialogs/VersionsDialog";
 
 export default {
   components: {
-    VersionsDialog
+    VersionsDialog,
   },
   created: function () {
-
-    if (this.$vuetify.lang.current == 'pt') {
+    if (this.$vuetify.lang.current == "pt") {
       this.brazilUnselected = false;
       this.usaUnselected = true;
     } else {
@@ -168,63 +163,65 @@ export default {
       this.usaUnselected = false;
     }
   },
-  data () {
+  data() {
     return {
       openVersionDialog: false,
       brazilUnselected: false,
       usaUnselected: true,
-      username: '',
-      image: '../assets/satelites.jpg',
+      username: "",
+      image: "../assets/satelites.jpg",
       loading: false,
       show: false,
-      password: '',
+      password: "",
       success: undefined,
       rules: {
-        emailMatch: () => this.success || this.success == undefined || this.$vuetify.lang.t('$vuetify.SENHA_INCORRETA'),
-        noProfile: () => (this.$getUser() && this.$getUser().perfis.length > 0) || this.$vuetify.lang.t('$vuetify.SEM_PERMISSAO')
+        emailMatch: () =>
+          this.success ||
+          this.success == undefined ||
+          this.$vuetify.lang.t("$vuetify.SENHA_INCORRETA"),
+        noProfile: () =>
+          this.$getUser().then((user) => {
+            return user.perfis.length > 0;
+          }) || this.$vuetify.lang.t("$vuetify.SEM_PERMISSAO"),
       },
-    }
+    };
   },
   methods: {
-    closeVersionsDialog () {
-      this.openVersionDialog = false
+    closeVersionsDialog() {
+      this.openVersionDialog = false;
     },
-    selectIdiom (country) {
-      if (country == 'brasil') {
-
+    selectIdiom(country) {
+      if (country == "brasil") {
         this.brazilUnselected = false;
         this.usaUnselected = true;
-        this.$vuetify.lang.current = 'pt'
-
-      } else if (country == 'usa') {
-
+        this.$vuetify.lang.current = "pt";
+      } else if (country == "usa") {
         this.brazilUnselected = true;
         this.usaUnselected = false;
-        this.$vuetify.lang.current = 'en'
-
+        this.$vuetify.lang.current = "en";
       }
 
-      this.$refs["password"].validate(false)
+      this.$refs["password"].validate(false);
     },
-    login () {
-
+    login() {
       this.loading = true;
-      this.$login(this.username, this.password).then(response => {
+      this.$login(this.username, this.password).then((response) => {
+        this.$getUser().then((user) => {
+          this.success = response;
+          if (this.success && user.perfis.length > 0) {
+            this.$root.$emit("login-success");
+          } else {
+            this.$refs["password"].validate(true);
+          }
 
-        this.success = response
-        if (this.success && this.$getUser().perfis.length > 0) {
-          this.$root.$emit('login-success')
-        } else {
-          this.$refs["password"].validate(true)
-        }
+          this.loading = false;
 
-        this.loading = false;
-
-        this.$saveOperation({ tipo: 'LOGIN', usuario: this.$getUser() })
-      })
-    }
-  }
-}
+          this.$saveOperation({ tipo: "LOGIN", usuario: user });
+        });
+      });
+    },
+  },
+};
 </script>
 <style scoped>
 .vertical-center {
