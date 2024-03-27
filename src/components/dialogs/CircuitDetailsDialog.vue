ad5b9ce6d3667a97958ef981479b05ed0488e409
JNobrega <camilaS0#>
2024-03-27 14:55:01 -0300

    IN-259 Adicionando labels para o eixo X.

--- src/components/dialogs/CircuitDetailsDialog.vue
+++ src/components/dialogs/CircuitDetailsDialog.vue
@@ -10,9 +10,19 @@
         style="word-break: normal; !important"
         :class="{ 'subtitle-2': $vuetify.breakpoint.xs }"
       >
-        {{ $vuetify.lang.t("$vuetify.MAIS_DETALHES") + " - " + 
-        circuit.nome + " ("+ $vuetify.lang.t("$vuetify.ULTIMA_ATUALIZACAO", 
-        this.$formatDate(circuit.updatedOn.date) + " " + this.$formatHour(circuit.updatedOn.time)) + ")"}}
+        {{
+          $vuetify.lang.t("$vuetify.MAIS_DETALHES") +
+          " - " +
+          circuit.nome +
+          " (" +
+          $vuetify.lang.t(
+            "$vuetify.ULTIMA_ATUALIZACAO",
+            this.$formatDate(circuit.updatedOn.date) +
+              " " +
+              this.$formatHour(circuit.updatedOn.time)
+          ) +
+          ")"
+        }}
       </v-card-title>
 
       <v-row class="mt-3 mb-10 justify-center">
@@ -102,32 +112,41 @@
       </v-row>
 
       <v-row class="mt-3 mb-10 justify-center">
-         <v-btn-toggle
-          mandatory
-          dense
-          color="blue"
-        >
+        <v-btn-toggle mandatory dense color="blue">
           <v-btn @click="searchMeasurements(0.25)">
-            <span class="hidden-sm-and-down caption">{{$vuetify.lang.t("$vuetify.MINUTOS_15")}}</span>
+            <span class="hidden-sm-and-down caption">{{
+              $vuetify.lang.t("$vuetify.MINUTOS_15")
+            }}</span>
           </v-btn>
           <v-btn @click="searchMeasurements(3)">
-            <span class="hidden-sm-and-down caption">{{$vuetify.lang.t("$vuetify.HORAS_3")}}</span>
+            <span class="hidden-sm-and-down caption">{{
+              $vuetify.lang.t("$vuetify.HORAS_3")
+            }}</span>
           </v-btn>
           <v-btn @click="searchMeasurements(24)">
-            <span class="hidden-sm-and-down caption">{{$vuetify.lang.t("$vuetify.HORAS_24")}}</span>
+            <span class="hidden-sm-and-down caption">{{
+              $vuetify.lang.t("$vuetify.HORAS_24")
+            }}</span>
           </v-btn>
-          <v-btn @click="searchMeasurements(24*7)">
-            <span class="hidden-sm-and-down caption">{{$vuetify.lang.t("$vuetify.DIAS_7")}}</span>
+          <v-btn @click="searchMeasurements(24 * 7)">
+            <span class="hidden-sm-and-down caption">{{
+              $vuetify.lang.t("$vuetify.DIAS_7")
+            }}</span>
           </v-btn>
-          <v-btn @click="searchMeasurements(24*30)">
-            <span class="hidden-sm-and-down caption">{{$vuetify.lang.t("$vuetify.DIAS_30")}}</span>
+          <v-btn @click="searchMeasurements(24 * 30)">
+            <span class="hidden-sm-and-down caption">{{
+              $vuetify.lang.t("$vuetify.DIAS_30")
+            }}</span>
           </v-btn>
         </v-btn-toggle>
       </v-row>
 
-      <v-row v-show="isLoading" class="mt-3 mb-3 ml-4 mr-4 justify-center" 
-            style="overflow-y:auto; height:40vh;" >
-         <v-progress-circular
+      <v-row
+        v-show="isLoading"
+        class="mt-3 mb-3 ml-4 mr-4 justify-center"
+        style="overflow-y: auto; height: 40vh"
+      >
+        <v-progress-circular
           :size="60"
           :width="3"
           color="red"
@@ -136,10 +155,15 @@
         ></v-progress-circular>
       </v-row>
 
-      <v-row v-show="!isLoading" class="mt-3 mb-3 ml-4 mr-4 justify-center" 
-            style="overflow-y:auto; max-height:40vh;" >
-        
-        <LineChart class="mb-10 mr-6" ref="chart1"
+      <v-row
+        v-show="!isLoading"
+        class="mt-3 mb-3 ml-4 mr-4 justify-center"
+        style="overflow-y: auto; max-height: 40vh"
+      >
+        <LineChart
+          v-if="!isLoading"
+          class="mb-10 mr-6"
+          ref="chart1"
           :options="downloadOptions"
           :styles="{
             height: '15rem',
@@ -158,8 +182,10 @@
             ],
           }"
         ></LineChart>
-       
-        <LineChart class="mr-6"
+
+        <LineChart
+          v-if="!isLoading"
+          class="mr-6"
           :options="uploadOptions"
           :styles="{
             height: '15rem',
@@ -178,8 +204,9 @@
             ],
           }"
         ></LineChart>
-       
+
         <LineChart
+          v-if="!isLoading"
           :options="latencyOptions"
           :styles="{
             height: '15rem',
@@ -198,8 +225,10 @@
             ],
           }"
         ></LineChart>
-       
-        <LineChart class="mr-6"
+
+        <LineChart
+          v-if="!isLoading"
+          class="mr-6"
           :options="packageOptions"
           :styles="{
             height: '15rem',
@@ -218,8 +247,10 @@
             ],
           }"
         ></LineChart>
-       
-        <LineChart class="mr-6"
+
+        <LineChart
+          v-if="!isLoading"
+          class="mr-6"
           :options="qualitySignalOptions"
           :styles="{
             height: '15rem',
@@ -238,15 +269,16 @@
             ],
           }"
         ></LineChart>
-      
-        <LineChart
+
+        <LineChart 
+        v-if="!isLoading"
           :options="obstructionOptions"
           :styles="{
             height: '15rem',
             width: '60vh',
           }"
           :chart-data="{
-            labels: ['1', '2', '3', '4', '5', '6', '7'],
+            labels: labels,
             datasets: [
               {
                 data: obstructionOptions.data,
@@ -258,7 +290,6 @@
             ],
           }"
         ></LineChart>
-       
       </v-row>
 
       <v-divider class="mt-0"></v-divider>
@@ -293,57 +324,68 @@ export default {
     circuit: Object,
   },
   watch: {
-    circuit(){
+    circuit() {
       this.searchMeasurements(0.25);
-    }
+    },
   },
   methods: {
-    loadDataToCharts(chartInfo){
-      for(var index in chartInfo){
+    loadDataToCharts(chartInfo) {
+      for (var index in chartInfo) {
         var element = chartInfo[index];
-        this.downloadOptions.data.push(element['taxaDownload']);
-        this.uploadOptions.data.push(element['taxaUpload']);
-        this.latencyOptions.data.push(element['latencia']);
-        this.packageOptions.data.push(element['taxaPerdaPing'] * 100);
-        this.qualitySignalOptions.data.push(element['qualidadeSinal'] * 100);
-        this.obstructionOptions.data.push(element['tempoObstrucao']);
+        this.downloadOptions.data.push(element["taxaDownload"]);
+        this.uploadOptions.data.push(element["taxaUpload"]);
+        this.latencyOptions.data.push(element["latencia"]);
+        this.packageOptions.data.push(element["taxaPerdaPing"] * 100);
+        this.qualitySignalOptions.data.push(element["qualidadeSinal"] * 100);
+        this.obstructionOptions.data.push(element["tempoObstrucao"]);
       }
     },
-    searchMeasurements(hours){
+    searchMeasurements(hours) {
+      this.isLoading = true;
+      this.$get("/medicao/busca", {
+        designationTpz: this.circuit.nome,
+        hours: hours,
+      }).then((response) => {
+        this.loadDataToCharts(response.data);
+        this.configureAxeLabels(hours, response.data);
+        this.isLoading = false;
+      });
+    },
+    formatHourAxeLabels(chartInfo){
 
-       this.isLoading = true;
-       this.$get("/medicao/busca", {
-          designationTpz: this.circuit.nome,
-          hours: hours,
-        }).then((response) => {
-          
-          this.isLoading = false;
-          this.loadDataToCharts(response.data);
-        });
+      this.labels = [];
+      for (var index in chartInfo) {
+        this.labels.push(this.$formatHour(chartInfo[index].createdOn.time, true));
+      }
+    },
+    formatDayAxeLabels(chartInfo){
 
-        if(hours == 0.25){
-          this.labels = ['0', '5 mins', '10 mins', '15 mins'];
-        }
+      this.labels = [];
+      for (var index in chartInfo) {
+        this.labels.push(this.$formatDate(chartInfo[index].createdOn.date));
+      }
+    },
+    configureAxeLabels(hours, chartInfo) {
 
-        if(hours == 3){
-          this.labels = ['0', '1h', '2h', '3h'];
-        }
+      if (hours == 0.25) {
+       this.formatHourAxeLabels(chartInfo);
+      }
 
-        if(hours == 24){
-          this.labels = ['0', '1h', '2h', '3h', '4h', '5h', '6h', '7h', '8h', '9h', '10h',
-                   '11h', '12h', '13h', '14h', '15h', '16h', '17h', '18h', '19h', '20h',
-                   '21h', '22h', '23h', '24h'];
-        }
+      if (hours == 3) {
+       this.formatHourAxeLabels(chartInfo);
+      }
 
-        if(hours == (7*24)){
-          this.labels = ['0', '1d', '2d', '3d', '4d', '5d', '6d', '7d'];
-        }
+      if (hours == 24) {
+        this.formatHourAxeLabels(chartInfo);
+      }
 
-        if(hours == (30*24)){
-          this.labels = ['0', '1d', '2d', '3d', '4d', '5d', '6d', '7d', '8d', '9d', '10d',
-          '11d', '12d', '13d', '14d', '15d', '16d', '17d', '18d', '19d', '20d',
-          '21d', '22d', '23d', '24d', '25d', '26d', '27d', '28d', '29d', '30d'];
-        }
+      if (hours == 7 * 24) {
+        this.formatDayAxeLabels(chartInfo)
+      }
+
+      if (hours == 30 * 24) {
+        this.formatDayAxeLabels(chartInfo)
+      }
     },
   },
   data: () => ({
@@ -354,49 +396,62 @@ export default {
     packageOptions: undefined,
     qualitySignalOptions: undefined,
     obstructionOptions: undefined,
-    labels: undefined,
+    labels: [],
     isLoading: false,
   }),
   created: function () {
     this.mainOptions = {
-        responsive: true,
-        maintainAspectRatio: false,
-        scales: {
-          yAxes: [{
+      responsive: true,
+      maintainAspectRatio: false,
+      scales: {
+        yAxes: [
+          {
             gridLines: { drawOnChartArea: false },
-          }],
-          xAxes: [{
-            gridLines: { drawOnChartArea: false },
-          }]
-        },
-        title: {
-          display: true,
-          fontSize: 18,
-        },
-        legend: {display:false},
-        elements: {
-          point: {
-            radius: 2,
           },
-          line: {
-            borderWidth: 2,
-          }
+        ],
+        xAxes: [
+          {
+            gridLines: { drawOnChartArea: false },
+          },
+        ],
+      },
+      title: {
+        display: true,
+        fontSize: 18,
+      },
+      legend: { display: false },
+      elements: {
+        point: {
+          radius: 2,
         },
-        data: [],
-    }
+        line: {
+          borderWidth: 2,
+        },
+      },
+      data: [],
+    };
 
-    this.downloadOptions = JSON.parse(JSON.stringify(this.mainOptions))
-    this.downloadOptions.title.text = this.$vuetify.lang.t('$vuetify.TAXA_DOWNLOAD');
-    this.uploadOptions = JSON.parse(JSON.stringify(this.mainOptions))
-    this.uploadOptions.title.text = this.$vuetify.lang.t('$vuetify.TAXA_UPLOAD');
-    this.latencyOptions = JSON.parse(JSON.stringify(this.mainOptions))
-    this.latencyOptions.title.text = this.$vuetify.lang.t('$vuetify.LATENCIA');
-    this.packageOptions = JSON.parse(JSON.stringify(this.mainOptions))
-    this.packageOptions.title.text = this.$vuetify.lang.t('$vuetify.TAXA_PERDA_PACOTES');
-    this.qualitySignalOptions = JSON.parse(JSON.stringify(this.mainOptions))
-    this.qualitySignalOptions.title.text = this.$vuetify.lang.t('$vuetify.QUALIDADE_SINAL') + " (%)";
-    this.obstructionOptions = JSON.parse(JSON.stringify(this.mainOptions))
-    this.obstructionOptions.title.text = this.$vuetify.lang.t('$vuetify.TEMPO_OBSTRUCAO');
+    this.downloadOptions = JSON.parse(JSON.stringify(this.mainOptions));
+    this.downloadOptions.title.text = this.$vuetify.lang.t(
+      "$vuetify.TAXA_DOWNLOAD"
+    );
+    this.uploadOptions = JSON.parse(JSON.stringify(this.mainOptions));
+    this.uploadOptions.title.text = this.$vuetify.lang.t(
+      "$vuetify.TAXA_UPLOAD"
+    );
+    this.latencyOptions = JSON.parse(JSON.stringify(this.mainOptions));
+    this.latencyOptions.title.text = this.$vuetify.lang.t("$vuetify.LATENCIA");
+    this.packageOptions = JSON.parse(JSON.stringify(this.mainOptions));
+    this.packageOptions.title.text = this.$vuetify.lang.t(
+      "$vuetify.TAXA_PERDA_PACOTES"
+    );
+    this.qualitySignalOptions = JSON.parse(JSON.stringify(this.mainOptions));
+    this.qualitySignalOptions.title.text =
+      this.$vuetify.lang.t("$vuetify.QUALIDADE_SINAL") + " (%)";
+    this.obstructionOptions = JSON.parse(JSON.stringify(this.mainOptions));
+    this.obstructionOptions.title.text = this.$vuetify.lang.t(
+      "$vuetify.TEMPO_OBSTRUCAO"
+    );
   },
 };
 </script>
