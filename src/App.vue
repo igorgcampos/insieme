6cc079e080e94ea713ef66dce10dfa6a8c50d4bb
JNobrega <camilaS0#>
2024-07-08 11:52:32 -0300

    Autoreconnect de conexão websocket.

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,7 @@
 <template>
-  <v-app style="background:#FAFAFA">
+  <v-app style="background: #fafafa">
     <router-view></router-view>
-    <ReportSideBar>
-    </ReportSideBar>
+    <ReportSideBar> </ReportSideBar>
   </v-app>
 </template>
 
@@ -21,60 +20,63 @@
 }
 </style>
 <script>
-
-import ReportSideBar from './components/ReportSideBar'
+import ReportSideBar from "./components/ReportSideBar";
 import SockJS from "sockjs-client";
 import Stomp from "webstomp-client";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    ReportSideBar
+    ReportSideBar,
   },
   created: function () {
-
     this.$router.beforeEach((to, from, next) => {
-      if (to.name !== 'Login' && !this.$isAuthenticated()) next({ name: 'Login' })
-      else if (to.name == 'Login' && this.$isAuthenticated()) next({ name: 'Main' })
-      else next()
-    })
+      if (to.name !== "Login" && !this.$isAuthenticated())
+        next({ name: "Login" });
+      else if (to.name == "Login" && this.$isAuthenticated())
+        next({ name: "Main" });
+      else next();
+    });
 
     this.setUrlToken().then(() => {
-
       this.$vuetify.lang.current = window.sessionStorage.getItem("idioma");
 
-      if(!window.sessionStorage.getItem('keyCloakToken')){
-        this.$router.push('/login')
-      }else{
-        this.$router.push('/contratos');
-        window.sessionStorage.setItem('actualPage', 'contratos')
+      if (!window.sessionStorage.getItem("keyCloakToken")) {
+        this.$router.push("/login");
+      } else {
+        this.$router.push("/contratos");
+        window.sessionStorage.setItem("actualPage", "contratos");
       }
 
       this.closeWebSocket();
       this.initiateWebSocket();
 
-      this.$root.$on('login-success', () => {
-        this.$router.push('/');
+      this.$root.$on("login-success", () => {
+        this.$router.push("/");
         this.initiateWebSocket();
-        window.sessionStorage.setItem('actualPage', '/')
-      })
-      this.$root.$on('logout-success', () => {
-        this.closeWebSocket(); 
-        this.$router.push('/login') 
+        window.sessionStorage.setItem("actualPage", "/");
+      });
+      this.$root.$on("logout-success", () => {
+        this.closeWebSocket();
+        this.$router.push("/login");
       });
     });
-
+  },
+  data() {
+    return {
+      intervalNumber: undefined,
+    };
   },
   methods: {
-    async setUrlToken () {
+    async setUrlToken() {
       let uri = window.location.search.substring(1);
       let params = new URLSearchParams(uri);
 
       if (params.get("token")) {
-        await this.$setKeycloakToken(params.get("token"))
+        await this.$setKeycloakToken(params.get("token"));
       }
     },
-     closeWebSocket() {
+    closeWebSocket() {
       if (this.stompClient && this.subscription) {
         this.subscription.unsubscribe();
         this.stompClient.disconnect();
@@ -84,28 +86,33 @@ export default {
       this.socket = new SockJS(
         process.env.VUE_APP_WEBSOCKET_URL + "/websocket-server"
       );
+
       this.stompClient = Stomp.over(this.socket);
       this.stompClient.debug = () => {};
 
-      this.$getUser().then(user => {
-
-        var channelName = (this.$hasProfile('Administrador')? 
-            'admin':user.contrato.id)
+      this.$getUser().then((user) => {
+        var channelName = this.$hasProfile("Administrador")
+          ? "admin"
+          : user.contrato.id;
 
         this.connect(channelName);
-      })
+      });
     },
     connect(channelName) {
       this.stompClient.connect(
         {},
         () => {
-          this.subscription = this.stompClient.subscribe("/topic/messages/" + channelName, (tick) => {
-            var receivedMessage = JSON.parse(tick.body);
-            this.$root.$emit(receivedMessage.type, receivedMessage);
-          });
+          console.log("Conexão websocket realizada com sucesso!");
+          this.subscription = this.stompClient.subscribe(
+            "/topic/messages/" + channelName,
+            (tick) => {
+              var receivedMessage = JSON.parse(tick.body);
+              this.$root.$emit(receivedMessage.type, receivedMessage);
+            }
+          );
         },
-        (error) => {
-          console.log(error);
+        () => {
+          console.log("Conexão websocket perdida!");
           if (!this.loggedOut) {
             this.stompClient.disconnect();
             this.socket = new SockJS(
@@ -113,11 +120,12 @@ export default {
             );
             this.stompClient = Stomp.over(this.socket);
             this.stompClient.debug = () => {};
+            
             this.connect(channelName);
           }
         }
       );
     },
-  }
+  },
 };
 </script>
